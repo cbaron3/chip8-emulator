0997dde5fea1de2ef0f96d296ea912b7aa96eb41
Carl Baron <carbaro196@gmail.com>
2019-01-30 21:09:01 -0500

    Added roms for testing. Added the ability to load in the binary roms into a vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,67 @@
 // Main file
 #include <iostream>
-#include "../include/Memory.h"
+#include <fstream>
+#include <vector>
+#include <string>
 
+#include "../include/Memory.h"
 #include "../include/Logger.h"
 
 
-int main(){
-	std::cout << "Running main function..." << std::endl;
+std::vector<uint8_t> load_rom(std::string file_name);
 
-	chip8::Memory memory;
-	memory.store_byte(0xF);
-	std::cout<<memory.get_memory_pointer();
-	memory.read_byte(0);
+int main(int argc, char **argv){
+	std::cout << "Running main function..." << std::endl;
+	util::Logger::getInstance()->set_max_log_level(LOGTYPE::DEBUG);
+
+	switch (argc) {
+	    case 1:
+	    {
+	      std::cout << "No CL arguments supplied" << std::endl;
+	      break;
+	    }
+	    case 2:
+	    {
+	   	  std::string s(argv[1]);
+	      util::LOG(LOGTYPE::DEBUG, s);
+	      break;
+	    }
+	    default:
+	    {
+	      std::cout << "Invalid CL arguments supplied" << std::endl;
+	      break;
+	  }
+	}
+
+	std::vector<uint8_t> rom = load_rom("../roms/full_games/PONG");
+
+	for(auto it = rom.begin(); it != rom.end(); ++it){
+		std::cout << (unsigned int)*it << " ";
+	}
 
 	std::cout << "Ending main function..." << std::endl;
 	return 0;
+}
+
+std::vector<uint8_t> load_rom(std::string file_name){
+	std::vector<uint8_t> rom;
+
+	std::ifstream f_rom(file_name);
+	
+	if(f_rom.is_open()){
+		unsigned char c = f_rom.get();
+
+		while(f_rom.good()){
+			uint8_t data = (uint8_t) c;
+			rom.push_back(data);
+			c = f_rom.get();
+		}
+	}
+	else{
+		//util::LOG(LOGTYPE::ERROR, "File: " + file_name + " failed to open.");
+	}
+
+	f_rom.close();
+
+	return rom;
 }
