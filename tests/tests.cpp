c8f9ee1cd41064727a215b00fe391952a278040a
Carl Baron <carbaro196@gmail.com>
2019-03-29 14:21:15 -0400

    Removed Memory test for unit tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,29 +2,6 @@
 #include "../src/Memory.cpp"
 #include <gtest/gtest.h>
 
-
-// Test to make sure the byte we store is actually stored properly
-TEST(MemoryTest, SingleStoreThenRead){
-	util::Logger::getInstance()->set_max_log_level(LOGTYPE::NONE);
-
-	chip8::Memory memory;
-	memory.store_byte(1);
-	ASSERT_EQ(1, memory.read_byte(memory.get_memory_pointer()-1));
-
-	memory.store_byte(0xFF);
-	ASSERT_EQ(0xFF, memory.read_byte(memory.get_memory_pointer()-1));
-}
-
-// Test to make sure we cannot access a value outside of the memory space
-TEST(MemoryTest, AccessibleAddress){
-	util::Logger::getInstance()->set_max_log_level(LOGTYPE::NONE);
-
-	chip8::Memory memory;
-	ASSERT_ANY_THROW(memory.read_byte(-1));
-	ASSERT_ANY_THROW(memory.read_byte(chip8::MEM_SPACE));
-	ASSERT_NO_THROW(memory.read_byte(chip8::MEM_SPACE-1));
-}
-
 TEST(MemoryTest, MemoryMap)
 {
 	chip8::MemoryMap memory(10, 1);
