b5affa2f16ee376ea1627fad2d5ca452bc50ce5f
Carl Baron <carbaro196@gmail.com>
2019-04-27 14:16:56 -0400

    stuff works

--- include/Interpreter.h
+++ include/Interpreter.h
@@ -5,152 +5,115 @@
 #include <stack>
 #include <iostream>
 #include "Chip8.h"
-#include <memory>           // Memory for unique ptr
+#include <memory> // Memory for unique ptr
 
 #include "Memory.h"
 
 namespace chip8
 {
-	class Interpreter
+class Interpreter
+{
+
+  public:
+	// Factory method
+	static std::unique_ptr<Interpreter> make_interpreter(std::unique_ptr<MemoryMap> memory);
+
+	// Interpreter next instruction
+	void next_instruction(void);
+
+	// Delay timer get
+	unsigned int delay(void) const { return m_delay_timer; }
+
+	// Sound timer get
+	unsigned int sound(void) const { return m_sound_timer; }
+
+	// Screen pixel get. uint32_t so it can be used
+	std::array<uint32_t, 64 * 32> screen(void) { return m_pixels; }
+
+	// Set keys externally
+	void sync_keys(std::array<bool, 16> t_keys) { this->m_keys = t_keys; }
+
+	// Exit program flag
+	bool exit(void) const { return m_exit_flag; }
+
+	// Check if we need to update the screen. Clear flag afterwards
+	bool draw(void)
 	{
+		bool temp_flag = m_draw_flag;
+		m_draw_flag = false;
+		return temp_flag;
+	}
 
-	public:
-		// Factory method
-		static std::unique_ptr<Interpreter> make_interpreter( std::unique_ptr<MemoryMap> memory );
+  private:
+	// Default constructor
+	Interpreter(void);
 
-		// Interpreter next instruction
-		void next_instruction( void );
+	// Private constructor to enforce unique pointer factory method
+	Interpreter(std::unique_ptr<MemoryMap> memory);
 
-		/* Shouldn't need setters cause we should have the outside scope modify the interpreter scope */
+	// Execute an opcode
+	void execute(const unsigned int &opcode);
 
-		// Delay timer get
-		unsigned int delay( void ) const { return m_delay_timer; }
+	// Interpreter's memory map to pull instructions from
+	std::unique_ptr<MemoryMap> memory_map;
 
-		// Sound timer get
-		unsigned int sound( void ) const { return m_sound_timer; } 
+	// Flag for exit
+	bool m_exit_flag, m_draw_flag;
 
-		// Program counter get
-		unsigned int pc( void ) const { return m_program_counter; }
+	// Timers, index register, program counter
+	unsigned int m_delay_timer, m_sound_timer, m_index_register, m_program_counter;
 
-		// Index register get
-		unsigned int ir( void ) const { return m_index_register; }
+	// Screen
+	std::array<uint32_t, 64 * 32> m_pixels;
 
-		// Screen pixel get
-		std::array<uint32_t, 2048> screen( void ) { return pixels; }
+	// Key pressed state
+	std::array<bool, 16> m_keys;
 
-		// Screen pixel print
-		void debug_screen( void )
-		{
-			for(int i = 0; i <2048; ++i)
-			{
-			      		
-			      std::cout << pixels[i] << " ";
+	// Register values
+	std::array<uint8_t, 16> m_registers;
 
-			      if(i%64 == 0)
-			      {
-			      	std::cout << std::endl;
-			      }
-			      
-			  }
-		}
-		
+	// Subroutine stack
+	unsigned int m_sp;
+	std::array<uint16_t, 16> m_stack;
+	
 
-		// Screen pixel get
-		std::array<uint8_t, 16> register_map( void ) { return registers; }
+	/* CPU OPCODE FUNCTION DEFINITIONS BELOW */
+	typedef void (*OpcodeTable)(Interpreter *cpu, const unsigned int &opcode);
 
-		// Set keys externally
-		void sync_keys(std::array<bool, 16> t_keys){ this->keys = t_keys; /* debug_keys(); */ }
+	OpcodeTable opcodes[16];
 
-		// Exit program flag
-		bool exit( void ) const { return m_exit_flag; }
+	static void opcode_0xxx(Interpreter *cpu, const unsigned int &opcode);
 
-		// Check if we need to update the screen
-		bool draw( void ) 
-		{ 
-			bool temp_flag = m_draw_flag;
-			m_draw_flag = false;
-			return temp_flag; 
-		}
+	static void opcode_1nnn(Interpreter *cpu, const unsigned int &opcode);
 
-		// TODO: ostream operator for printing class contents
+	static void opcode_2nnn(Interpreter *cpu, const unsigned int &opcode);
 
-	private:
-		// Default constructor
-		Interpreter( void );
+	static void opcode_3xnn(Interpreter *cpu, const unsigned int &opcode);
 
-		// Private constructor to enforce unique pointer factory method
-		Interpreter(std::unique_ptr<MemoryMap> memory);
+	static void opcode_4xnn(Interpreter *cpu, const unsigned int &opcode);
 
-		void debug_keys( void )
-		{
-			for(int i = 0; i < 16; ++i)
-			{
-				std::cout << chip8::sdl_key_strings[i] << " " << std::boolalpha << keys[i] << std::endl;
-			}
-		}
+	static void opcode_5xy0(Interpreter *cpu, const unsigned int &opcode);
 
-		// Execute an opcode
-		void execute( const unsigned int& opcode );
+	static void opcode_6xnn(Interpreter *cpu, const unsigned int &opcode);
 
-		// Interpreter's memory map to pull instructions from
-		std::unique_ptr<MemoryMap> memory_map; 
+	static void opcode_7xnn(Interpreter *cpu, const unsigned int &opcode);
 
-		// Flag for exit
-		bool m_exit_flag, m_draw_flag;
+	static void opcode_8XYx(Interpreter *cpu, const unsigned int &opcode);
 
-		// Timers, index register, program counter
-		unsigned int m_delay_timer, m_sound_timer, m_index_register, m_program_counter;
+	static void opcode_9xy0(Interpreter *cpu, const unsigned int &opcode);
 
-		// Screen
-		std::array<uint32_t, 64*32> pixels;
+	static void opcode_Annn(Interpreter *cpu, const unsigned int &opcode);
 
-		// Key pressed state
-		std::array<bool, 16> keys;
+	static void opcode_Bxnn(Interpreter *cpu, const unsigned int &opcode);
 
-		// Register values
-		std::array<uint8_t, 16> registers;
+	static void opcode_Cxnn(Interpreter *cpu, const unsigned int &opcode);
 
-		// Subroutine stack
-		std::array<uint16_t, 16> subroutine_stack;
-		uint16_t sp;
+	static void opcode_Dxyn(Interpreter *cpu, const unsigned int &opcode);
 
-		/* CPU OPCODE FUNCTION DEFINITIONS BELOW */
-		typedef void(*OpcodeTable)( Interpreter* cpu, const unsigned int& opcode );
+	static void opcode_EXxx(Interpreter *cpu, const unsigned int &opcode);
 
-		OpcodeTable opcodes[16];
-
-		static void opcode_0xxx( Interpreter* cpu, const unsigned int& opcode );
-
-		static void opcode_1nnn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_2nnn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_3xnn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_4xnn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_5xy0( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_6xnn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_7xnn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_8XYx( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_9xy0( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_Annn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_Bxnn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_Cxnn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_Dxyn( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_EXxx( Interpreter* cpu, const unsigned int& opcode );
-		
-		static void opcode_FXxx( Interpreter* cpu, const unsigned int& opcode );
-	};
-}
+	static void opcode_FXxx(Interpreter *cpu, const unsigned int &opcode);
+};
+} // namespace chip8
 
 #endif // CHIP8_CPU_H
